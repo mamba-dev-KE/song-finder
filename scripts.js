f88b4917041830b680702fd926d7c70fb0902d9b
mamba-dev-KE <josephmaramba50@gmail.com>
2021-08-30 06:38:38 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,54 @@
+let searchTerm = "";
+const songContainer = document.getElementById("songs");
+
+// search for songs from different artists
+const updateSearchTerm = () => {
+	searchTerm = document.getElementById("search").value;
+	if (!searchTerm || searchTerm === "") {
+		alert("input a search string");
+	} else {
+		//check if songcontainer has child elements
+		while (songContainer.firstChild) {
+			songContainer.removeChild(songContainer.firstChild);
+		}
+		//fetch songs from iTunes search API
+		const url = `https://itunes.apple.com/search?media=music&limit=10&term=${searchTerm}`;
+		fetch(url)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data.results);
+				const artists = data.results;
+				return artists.map((result) => {
+					// declaring variables that hold created HTML elements
+					const article = document.createElement("article"),
+						artist = document.createElement("h2"),
+						song = document.createElement("p"),
+						img = document.createElement("img"),
+						audio = document.createElement("audio"),
+						audioSource = document.createElement("source");
+
+					// setting declared variables to data derived from API
+					artist.innerHTML = result.artistName;
+					song.innerHTML = result.trackName;
+					img.src = result.artworkUrl100;
+					audioSource.src = result.previewUrl;
+					audio.setAttribute("controls", "");
+
+					//appending created elements as children of the html article
+					article.appendChild(artist);
+					article.appendChild(song);
+					article.appendChild(img);
+					article.appendChild(audio);
+					audio.appendChild(audioSource);
+					songContainer.appendChild(article);
+				});
+			})
+			.catch((error) => {
+				console.log("Request failed: ", error);
+			});
+	}
+};
+
+//event listener for a search event on button
+const searchBtn = document.querySelector("button");
+searchBtn.addEventListener("click", updateSearchTerm);
